b75dffdef33756f4286100ca6a1bc36aabe830cc
mahmoud-077 <mahmoudadel537@gmail.com.com>
2024-05-14 02:02:43 +0300

    commit#22

---
+++ example_21/src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
@@ -0,0 +1,52 @@
+package com.eazybytes.eazyschool.controller;
+
+import com.eazybytes.eazyschool.model.Contact;
+import com.eazybytes.eazyschool.service.ContactService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+@Controller
+public class ContactController {
+    private ContactService contactService;
+
+
+    @Autowired
+    public ContactController(ContactService contactService) {
+        this.contactService = contactService;
+    }
+
+    @RequestMapping("/contact")
+    public String displayContactPage() {
+        return "contact.html";
+    }
+
+//    @RequestMapping(value = "/saveMsg",method = RequestMethod.POST)
+//    public String saveMessage(@RequestParam String name, @RequestParam String mobileNum,
+//                                    @RequestParam String email, @RequestParam String subject,
+//                                    @RequestParam String message) {
+//
+//        log.info("Name: " + name);
+//        log.info("Mobile Number: " + mobileNum);
+//        log.info("Email: " + email);
+//        log.info("Subject: " + subject);
+//        log.info("Message: " + message);
+//
+////        return new ModelAndView("redirect:/contact");
+//        return "redirect:/contact";
+//    }
+
+    @PostMapping(value = "/saveMsg")
+    public ModelAndView saveMessage(Contact contact) {
+        contactService.saveMessageDetails(contact);
+        return new ModelAndView("redirect:/contact");
+    }
+}
