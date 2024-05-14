db763fb7cea9a27ebee3b2a7646ace49af2f7b4a
mahmoud-077 <mahmoudadel537@gmail.com.com>
2024-05-15 01:16:43 +0300

    commit#23

---
+++ example_22/src/main/java/com/eazybytes/eazyschool/service/ContactService.java
@@ -0,0 +1,17 @@
+package com.eazybytes.eazyschool.service;
+
+import com.eazybytes.eazyschool.model.Contact;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class ContactService {
+
+
+    public boolean saveMessageDetails(Contact contact) {
+        boolean isSaved = true;
+        log.info(contact.toString());
+        return isSaved;
+    }
+}
