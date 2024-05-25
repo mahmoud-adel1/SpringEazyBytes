03001fd7458c17835bfa8ee19f5bc3e9044cda00
mahmoud-077 <mahmoudadel537@gmail.com.com>
2024-05-26 02:03:29 +0300

    commit#36

---
+++ example_36/src/main/java/com/eazybytes/eazyschool/repository/HolidayRepository.java
@@ -0,0 +1,39 @@
+package com.eazybytes.eazyschool.repository;
+
+import com.eazybytes.eazyschool.model.Holiday;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HolidayRepository extends CrudRepository<Holiday,String> {
+
+}
+
+
+
+//import com.eazybytes.eazyschool.model.Holiday;
+//import com.eazybytes.eazyschool.rowmappers.HolidayRowMapper;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.jdbc.core.BeanPropertyRowMapper;
+//import org.springframework.jdbc.core.JdbcTemplate;
+//import org.springframework.stereotype.Repository;
+//
+//import javax.swing.tree.RowMapper;
+//import java.util.List;
+
+//@Repository
+//public class HolidayRepository {
+//    private JdbcTemplate jdbcTemplate;
+//
+//    @Autowired
+//    public HolidayRepository(JdbcTemplate jdbcTemplate) {
+//        this.jdbcTemplate = jdbcTemplate;
+//    }
+//
+//    public List<Holiday> findAll() {
+//        String sql = "SELECT * FROM eazyschool.holidays";
+//        var mapper = BeanPropertyRowMapper.newInstance(Holiday.class);
+//        return jdbcTemplate.query(sql,mapper);
+//    }
+//}
+
