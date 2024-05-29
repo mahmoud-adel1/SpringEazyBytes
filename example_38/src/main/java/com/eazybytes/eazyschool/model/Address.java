20a2c893faad97a0fc6d3df980ae12cab9101651
mahmoud-077 <mahmoudadel537@gmail.com.com>
2024-05-29 13:46:13 +0300

    commit#38

---
+++ example_38/src/main/java/com/eazybytes/eazyschool/model/Address.java
@@ -0,0 +1,40 @@
+package com.eazybytes.eazyschool.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "address")
+public class Address extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "address_id")
+    private int addressId;
+
+    @NotBlank(message = "Address1 must not be blank")
+    @Size(min = 5, message = "Address1 must be at least 5 characters long")
+    @Column(name = "address1")
+    private String addressOne;
+
+    @Column(name = "address2")
+    private String addressTwo;
+
+    @NotBlank(message = "City must not be blank")
+    @Size(min = 5, message = "City must be at least 5 characters long")
+    @Column(name = "city")
+    private String city;
+
+    @NotBlank(message = "State must not be blank")
+    @Size(min = 5, message = "State must be at least 5 characters long")
+    @Column(name = "state")
+    private String state;
+
+    @Column(name = "zip_code")
+    @Pattern(regexp = "(^$|[0-9]{5})", message = "Zip Code must be at least 5 digits")
+    private int zipCode;
+
+}
---
+++ example_38/src/main/java/com/eazybytes/eazyschool/security/EazySchoolUsernamePwdAuthenticationProvider.java
@@ -0,0 +1,53 @@
+package com.eazybytes.eazyschool.security;
+
+import com.eazybytes.eazyschool.model.Person;
+import com.eazybytes.eazyschool.model.Role;
+import com.eazybytes.eazyschool.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Component
+public class EazySchoolUsernamePwdAuthenticationProvider implements AuthenticationProvider {
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public EazySchoolUsernamePwdAuthenticationProvider(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+        String email = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        Person person = personRepository.getPersonByEmail(email);
+        if(null != person && person.getPersonId()>0 && password.equals(person.getPassword())) {
+            return new UsernamePasswordAuthenticationToken(person.getName(),password,getGrantedAuthorities(person.getRole()));
+        } else {
+            throw new BadCredentialsException("Invalid credentials");
+        }
+    }
+
+    private Collection<? extends GrantedAuthority> getGrantedAuthorities(Role role) {
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_"+role.getRoleName()));
+        return grantedAuthorities;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
